935152f5fbdb5b8db3361b29b5f6def1c38acec0
Abdullah Akbar <Akbar.A@etoncollege.org.uk>
2023-02-04 10:47:24 +0000

    Initial Commit

---
+++ CommentCards/CommentCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardsApp.swift
+//  CommentCards
+//
+//  Created by Akbar, Abdullah (RCH) on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
